26f4054c6c77e95100fcf60b067e72c291b8bc10
ARIJIT P S <arijit.p.17003@iitgoa.ac.in>
2020-06-21 22:13:01 +0530

    second commit, switching activity between signup and log in, keyboard goes down og clicking empty spaces, enter key activates sign up or log in

--- app/src/main/java/com/example/insta/LogIn.java
+++ app/src/main/java/com/example/insta/LogIn.java
@@ -3,6 +3,10 @@ package com.example.insta;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.tabs.TabLayout;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 import androidx.appcompat.widget.Toolbar;
 
 //<com.google.android.material.appbar.AppBarLayout/>
@@ -13,16 +17,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity implements View.OnClickListener {
 
     private EditText userName,password;
     private Button logIn;
     private TextView signUp;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +40,57 @@ public class LogIn extends AppCompatActivity {
 
         userName=findViewById(R.id.edtusername);
         password=findViewById(R.id.edtPassword);
+
+        password.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode==KeyEvent.KEYCODE_ENTER && event.getAction()==KeyEvent.ACTION_DOWN){
+                    onClick(logIn);
+                }
+                return false;
+            }
+        });
+
         logIn=findViewById(R.id.btnLogin);
         signUp=findViewById(R.id.txtSignup);
 
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        setTitle("Log In");
+
+        signUp.setOnClickListener(this);
+        logIn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin:
+                ParseUser.logInInBackground(userName.getText().toString() , password.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user!=null && e==null){
+                            Toast.makeText(LogIn.this,"Log in successful",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(LogIn.this,"Log in failed, please check your password",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                break;
+
+            case R.id.txtSignup:
                 Intent intent=new Intent(LogIn.this,SignUp.class);
                 startActivity(intent);
-            }
-        });
+                break;
+        }
+    }
+
+    public void layoutClicked(View view){
+        try{
+            InputMethodManager inputMethodManager= (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/insta/SignUp.java
+++ app/src/main/java/com/example/insta/SignUp.java
@@ -1,16 +1,111 @@
 package com.example.insta;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUp extends AppCompatActivity {
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
+    private EditText userName, passWord,userEmail;
+    private Button signUp;
+    private TextView logIn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.signup);
+
+        userName = findViewById(R.id.edtUserNameS);
+        passWord = findViewById(R.id.edtPasswordS);
+        userEmail = findViewById(R.id.edtEmail);
+
+        setTitle("Sign Up");
+
+        signUp = findViewById(R.id.btnSignUp);
+        logIn = findViewById(R.id.txtLogIn);
+
+        passWord.setOnKeyListener(this);
+
+        signUp.setOnClickListener(SignUp.this);
+        logIn.setOnClickListener(this);
+
+        if (ParseUser.getCurrentUser() != null) {
+            ParseUser.getCurrentUser().logOut();
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnSignUp:
+                if (passWord.getText().toString().equals("")
+                        || userEmail.getText().toString().equals("")
+                            || userEmail.getText().toString().equals("")) {
+                    Toast.makeText(SignUp.this, "Columns cannot be left empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    ParseUser user = new ParseUser();
+                    user.setEmail(userEmail.getText().toString());
+                    user.setUsername(userName.getText().toString());
+                    user.setPassword(passWord.getText().toString());
+
+                    final ProgressDialog progressDialog= new ProgressDialog(SignUp.this);
+                    progressDialog.setMessage("Signing in as "+ userName.getText());
+                    progressDialog.show();
+
+                    user.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toast.makeText(SignUp.this, "Successful", Toast.LENGTH_SHORT).show();
+
+
+                            } else {
+                                Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                            progressDialog.dismiss();
+                        }
+                    });
+
+                }
+                break;
+
+            case R.id.txtLogIn:
+                Intent intent=new Intent(SignUp.this,LogIn.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_ENTER && event.getAction()==KeyEvent.ACTION_DOWN){
+            onClick(signUp);
+        }
+        return false;
+    }
+
+    public void layoutTapped(View view){
+        try{
+            InputMethodManager inputMethodManager= (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
